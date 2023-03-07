76edc41bbacaee477e4b3f306f5c6e88b58285d6
ptdat4823 <phamtiendat4823@gmail.com>
2023-03-07 20:07:52 +0700

    change filename

--- index-js-string.js
+++ index-js-string.js
@@ -4,3 +4,8 @@ console.log(`my name is 'Dat'`);
 console.log(`my name is "Dat"`);
 console.log("my name is 'Dat'");
 console.log("my name is `Dat`");
+
+let x = 5, y = 6;
+
+console.log("x + y = ", x+y);
+console.log("this is \\");
